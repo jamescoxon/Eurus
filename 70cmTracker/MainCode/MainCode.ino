37d8460e6c443c3cc7754581589a83692bf961c7
James Coxon <jacoxon@googlemail.com>
2012-05-17 08:02:09 +0100

    added file structure and also put in basis for 70cmTracker

---
+++ 70cmTracker/MainCode/MainCode.ino
@@ -0,0 +1,601 @@
+/*
+* PicoAtlas - James Coxon jacoxon@googlemail.com
+* Experiemental pico payload based upon Arduino Mini Pro, RF22b and a Ublox GPS
+* Designed to be ultra lightweight, current hardware weighs 33g including batteries
+* and can be lifted with a single 92cm foil balloon. Designed to explore long duration
+* floats involving a degree of super-pressure.
+* Transmits RTTY during the day and Slow-Hell at night. See http://ukhas.org.uk/projects:picoatlas
+* for more info.
+*
+* Latest code can be found: https://github.com/jamescoxon/PicoAtlas
+*
+* GPS Code from jonsowman and Joey flight computer CUSF
+* https://github.com/cuspaceflight/joey-m/tree/master/firmware
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+#include <SPI.h>
+#include <RFM22.h>
+#include <util/crc16.h>
+
+//Setup radio on SPI with NSEL on pin 6
+rfm22 radio1(6);
+
+//Variables
+int32_t lat = 0, lon = 0, alt = 0;
+uint8_t hour = 0, minute = 0, second = 0, lock = 0, sats = 0;
+unsigned long startGPS = 0;
+int GPSerror = 0, count = 0, n, gpsstatus, lockcount = 0, battV = 0, intTemp = 0, oldLat = 0, total_time = -1, old_total_time = -2;
+
+uint8_t buf[60]; //GPS receive buffer
+char superbuffer [80]; //Telem string buffer
+
+// RTTY Functions - from RJHARRISON's AVR Code
+void rtty_txstring (char * string)
+{
+
+	/* Simple function to sent a char at a time to 
+	** rtty_txbyte function. 
+	** NB Each char is one byte (8 Bits)
+	*/
+	char c;
+	c = *string++;
+	while ( c != '\0')
+	{
+		rtty_txbyte (c);
+		c = *string++;
+	}
+}
+
+void rtty_txbyte (char c)
+{
+	/* Simple function to sent each bit of a char to 
+	** rtty_txbit function. 
+	** NB The bits are sent Least Significant Bit first
+	**
+	** All chars should be preceded with a 0 and 
+	** proceded with a 1. 0 = Start bit; 1 = Stop bit
+	**
+	** ASCII_BIT = 7 or 8 for ASCII-7 / ASCII-8
+	*/
+	int i;
+	rtty_txbit (0); // Start bit
+	// Send bits for for char LSB first	
+	for (i=0;i<8;i++)
+	{
+		if (c & 1) rtty_txbit(1); 
+			else rtty_txbit(0);	
+		c = c >> 1;
+	}
+	rtty_txbit (1); // Stop bit
+        rtty_txbit (1); // Stop bit
+}
+
+void rtty_txbit (int bit)
+{
+		if (bit)
+		{
+		  // high
+                  radio1.setFrequency(434.2015);
+		}
+		else
+		{
+		  // low
+                  radio1.setFrequency(434.201);
+		}
+                delayMicroseconds(19500); // 10000 = 100 BAUD 20150
+
+}
+
+//***************Hellschreiber******************
+
+struct t_htab { char c; int hellpat[5]; } ;
+
+struct t_htab helltab[] = {
+
+  {'1', { B00000100, B00000100, B01111100, B00000000, B00000000 } },
+  {'2', { B01001000, B01100100, B01010100, B01001100, B01000000 } },
+  {'3', { B01000100, B01000100, B01010100, B01010100, B00111100 } },
+  {'4', { B00011100, B00010000, B00010000, B01111100, B00010000 } },
+  {'5', { B01000000, B01011100, B01010100, B01010100, B00110100 } },
+  {'6', { B00111100, B01010010, B01001010, B01001000, B00110000 } },
+  {'7', { B01000100, B00100100, B00010100, B00001100, B00000100 } },
+  {'8', { B00111100, B01001010, B01001010, B01001010, B00111100 } },
+  {'9', { B00001100, B01001010, B01001010, B00101010, B00111100 } },
+  {'0', { B00111000, B01100100, B01010100, B01001100, B00111000 } },
+  {'A', { B01111000, B00101100, B00100100, B00101100, B01111000 } },
+  {'B', { B01000100, B01111100, B01010100, B01010100, B00101000 } },
+  {'C', { B00111000, B01101100, B01000100, B01000100, B00101000 } },
+  {'D', { B01000100, B01111100, B01000100, B01000100, B00111000 } },
+  {'E', { B01111100, B01010100, B01010100, B01000100, B01000100 } },
+  {'F', { B01111100, B00010100, B00010100, B00000100, B00000100 } },
+  {'G', { B00111000, B01101100, B01000100, B01010100, B00110100 } },
+  {'H', { B01111100, B00010000, B00010000, B00010000, B01111100 } },
+  {'I', { B00000000, B01000100, B01111100, B01000100, B00000000 } },
+  {'J', { B01100000, B01000000, B01000000, B01000000, B01111100 } },
+  {'K', { B01111100, B00010000, B00111000, B00101000, B01000100 } },
+  {'L', { B01111100, B01000000, B01000000, B01000000, B01000000 } },
+  {'M', { B01111100, B00001000, B00010000, B00001000, B01111100 } },
+  {'N', { B01111100, B00000100, B00001000, B00010000, B01111100 } },
+  {'O', { B00111000, B01000100, B01000100, B01000100, B00111000 } },
+  {'P', { B01000100, B01111100, B01010100, B00010100, B00011000 } },
+  {'Q', { B00111000, B01000100, B01100100, B11000100, B10111000 } },
+  {'R', { B01111100, B00010100, B00010100, B00110100, B01011000 } },
+  {'S', { B01011000, B01010100, B01010100, B01010100, B00100100 } },
+  {'T', { B00000100, B00000100, B01111100, B00000100, B00000100 } },
+  {'U', { B01111100, B01000000, B01000000, B01000000, B01111100 } },
+  {'V', { B01111100, B00100000, B00010000, B00001000, B00000100 } },
+  {'W', { B01111100, B01100000, B01111100, B01000000, B01111100 } },
+  {'X', { B01000100, B00101000, B00010000, B00101000, B01000100 } },
+  {'Y', { B00000100, B00001000, B01110000, B00001000, B00000100 } },
+  {'Z', { B01000100, B01100100, B01010100, B01001100, B01100100 } },
+  {'.', { B01000000, B01000000, B00000000, B00000000, B00000000 } },
+  {',', { B10000000, B10100000, B01100000, B00000000, B00000000 } },
+  {'/', { B01000000, B00100000, B00010000, B00001000, B00000100 } },
+  {'*', { B00000000, B00000000, B00000100, B00001110, B00000100 } }
+
+};
+
+#define N_HELL  (sizeof(helltab)/sizeof(helltab[0]))
+
+void helldelay()
+{
+  //Slow
+  delay(64);
+  delayMicroseconds(900);
+
+}
+
+void on()
+{
+  radio1.write(0x07, 0x08); //on
+  helldelay();
+  radio1.write(0x07, 0x01); //off
+}
+
+void hellsend(char c)
+{
+  int i, j, q ;
+  if (c == ' ') {
+      for (int d=0; d<14; d++){
+        helldelay();  
+      }
+    return ;
+  }
+  for (i=0; i<N_HELL; i++) {
+    if (helltab[i].c == c) {
+      //Serial.print(helltab[i].c) ;
+      
+      for (j=0; j<=4; j++) 
+      {
+        byte mask = B10000000;
+        for (q=0; q<=6; q++)
+        {      
+          if(helltab[i].hellpat[j] & mask) {
+            on();
+          } else {
+            helldelay();
+          }
+          mask >>= 1;
+        }
+      }
+      for (int d=0; d<14; d++){
+        helldelay();  
+      }
+      return ;
+    }
+  }
+  /* if we drop off the end, then we send a space */
+  //Serial.print("?") ;
+}
+
+void hellsendmsg(char *str)
+{
+  while (*str)
+    hellsend(*str++) ;
+  //Serial.println("");
+}
+
+// Send a byte array of UBX protocol to the GPS
+void sendUBX(uint8_t *MSG, uint8_t len) {
+  for(int i=0; i<len; i++) {
+    Serial.write(MSG[i]);
+  }
+  //Serial.println();
+}
+
+void setupRadio(){
+  
+  digitalWrite(7, LOW); // Turn on Radio
+  
+  delay(1000);
+  
+  rfm22::initSPI();
+
+  radio1.init();
+  
+  radio1.write(0x71, 0x00); // unmodulated carrier
+ 
+  //This sets up the GPIOs to automatically switch the antenna depending on Tx or Rx state, only needs to be done at start up
+  radio1.write(0x0b,0x12);
+  radio1.write(0x0c,0x15);
+  
+  radio1.setFrequency(434.201);
+  
+  //radio1.write(0x6D, 0x03);// turn tx low power 8db
+  radio1.write(0x6D, 0x04);// turn tx low power 11db
+  
+  radio1.write(0x07, 0x08); // turn tx on
+  delay(1000);
+  radio1.write(0x07, 0x01); // turn tx off
+  
+}
+//************Other Functions*****************
+
+uint16_t gps_CRC16_checksum (char *string)
+{
+	size_t i;
+	uint16_t crc;
+	uint8_t c;
+ 
+	crc = 0xFFFF;
+ 
+	// Calculate checksum ignoring the first two $s
+	for (i = 2; i < strlen(string); i++)
+	{
+		c = string[i];
+		crc = _crc_xmodem_update (crc, c);
+	}
+ 
+	return crc;
+}
+
+void setupGPS() {
+  //Turning off all GPS NMEA strings apart on the uBlox module
+  Serial.println("$PUBX,40,GLL,0,0,0,0*5C");
+  delay(1000);
+  Serial.println("$PUBX,40,GGA,0,0,0,0*5A");
+  delay(1000);
+  Serial.println("$PUBX,40,GSA,0,0,0,0*4E");
+  delay(1000);
+  Serial.println("$PUBX,40,RMC,0,0,0,0*47");
+  delay(1000);
+  Serial.println("$PUBX,40,GSV,0,0,0,0*59");
+  delay(1000);
+  Serial.println("$PUBX,40,VTG,0,0,0,0*5E");
+  delay(3000); // Wait for the GPS to process all the previous commands
+  
+}
+
+void PSMgps(){
+   setupGPS();
+   //set GPS to Eco mode (reduces current by 4mA)
+   uint8_t setEco[] = {0xB5, 0x62, 0x06, 0x11, 0x02, 0x00, 0x00, 0x04, 0x1D, 0x85};
+   sendUBX(setEco, sizeof(setEco)/sizeof(uint8_t));
+}
+
+void gpsPower(int i){
+  if(i == 0){
+    //turn off GPS
+    //  uint8_t GPSoff[] = {0xB5, 0x62, 0x02, 0x41, 0x08, 0x00, 0x00, 0x00, 0x00, 0x00, 0x02, 0x00, 0x00, 0x00, 0x4D, 0x3B};
+    uint8_t GPSoff[] = {0xB5, 0x62, 0x06, 0x04, 0x04, 0x00, 0x00, 0x00, 0x08, 0x00, 0x16, 0x74};
+    sendUBX(GPSoff, sizeof(GPSoff)/sizeof(uint8_t));
+    gpsstatus = 0;
+  }
+  else if (i == 1){
+    //turn on GPS
+    // uint8_t GPSon[] = {0xB5, 0x62, 0x02, 0x41, 0x08, 0x00, 0x00, 0x00, 0x00, 0x00, 0x01, 0x00, 0x00, 0x00, 0x4C, 0x37};
+    uint8_t GPSon[] = {0xB5, 0x62, 0x06, 0x04, 0x04, 0x00, 0x00, 0x00, 0x09, 0x00, 0x17, 0x76};
+    sendUBX(GPSon, sizeof(GPSon)/sizeof(uint8_t));
+    gpsstatus = 1;
+    delay(1000);
+    setupGPS();
+  }
+}
+
+/**
+ * Calculate a UBX checksum using 8-bit Fletcher (RFC1145)
+ */
+void gps_ubx_checksum(uint8_t* data, uint8_t len, uint8_t* cka,
+        uint8_t* ckb)
+{
+    *cka = 0;
+    *ckb = 0;
+    for( uint8_t i = 0; i < len; i++ )
+    {
+        *cka += *data;
+        *ckb += *cka;
+        data++;
+    }
+}
+
+/**
+ * Verify the checksum for the given data and length.
+ */
+bool _gps_verify_checksum(uint8_t* data, uint8_t len)
+{
+    uint8_t a, b;
+    gps_ubx_checksum(data, len, &a, &b);
+    if( a != *(data + len) || b != *(data + len + 1))
+        return false;
+    else
+        return true;
+}
+
+/**
+ * Get data from GPS, times out after 1 second.
+ */
+void gps_get_data()
+{
+    int i = 0;
+    unsigned long startTime = millis();
+    while (1) {
+    // Make sure data is available to read
+    if (Serial.available()) {
+      buf[i] = Serial.read();
+      i++;
+    }
+    // Timeout if no valid response in 1 seconds
+    if (millis() - startTime > 1000) {
+      break;
+    }
+    }
+}
+/**
+ * Check the navigation status to determine the quality of the
+ * fix currently held by the receiver with a NAV-STATUS message.
+ */
+void gps_check_lock()
+{
+    GPSerror = 0;
+    Serial.flush();
+    // Construct the request to the GPS
+    uint8_t request[8] = {0xB5, 0x62, 0x01, 0x06, 0x00, 0x00,
+        0x07, 0x16};
+    sendUBX(request, 8);
+
+    // Get the message back from the GPS
+    gps_get_data();
+    // Verify the sync and header bits
+    if( buf[0] != 0xB5 || buf[1] != 0x62 ) {
+      GPSerror = 11;
+    }
+    if( buf[2] != 0x01 || buf[3] != 0x06 ) {
+      GPSerror = 12;
+    }
+
+    // Check 60 bytes minus SYNC and CHECKSUM (4 bytes)
+    if( !_gps_verify_checksum(&buf[2], 56) ) {
+      GPSerror = 13;
+    }
+    
+    if(GPSerror == 0){
+    // Return the value if GPSfixOK is set in 'flags'
+    if( buf[17] & 0x01 )
+        lock = buf[16];
+    else
+        lock = 0;
+
+    sats = buf[53];
+    }
+    else {
+      lock = 0;
+    }
+}
+
+/**
+ * Poll the GPS for a position message then extract the useful
+ * information from it - POSLLH.
+ */
+void gps_get_position()
+{
+    GPSerror = 0;
+    Serial.flush();
+    // Request a NAV-POSLLH message from the GPS
+    uint8_t request[8] = {0xB5, 0x62, 0x01, 0x02, 0x00, 0x00, 0x03,
+        0x0A};
+    sendUBX(request, 8);
+    
+    // Get the message back from the GPS
+    gps_get_data();
+
+    // Verify the sync and header bits
+    if( buf[0] != 0xB5 || buf[1] != 0x62 )
+        GPSerror = 21;
+    if( buf[2] != 0x01 || buf[3] != 0x02 )
+        GPSerror = 22;
+        
+    if( !_gps_verify_checksum(&buf[2], 32) ) {
+      GPSerror = 23;
+    }
+    
+    if(GPSerror == 0) {
+      // 4 bytes of longitude (1e-7)
+      lon = (int32_t)buf[10] | (int32_t)buf[11] << 8 | 
+          (int32_t)buf[12] << 16 | (int32_t)buf[13] << 24;
+      //lon /= 1000;
+      
+      // 4 bytes of latitude (1e-7)
+      lat = (int32_t)buf[14] | (int32_t)buf[15] << 8 | 
+          (int32_t)buf[16] << 16 | (int32_t)buf[17] << 24;
+      //lat /= 1000;
+      
+      // 4 bytes of altitude above MSL (mm)
+      alt = (int32_t)buf[22] | (int32_t)buf[23] << 8 | 
+          (int32_t)buf[24] << 16 | (int32_t)buf[25] << 24;
+      alt /= 1000;
+    }
+
+}
+
+/**
+ * Get the hour, minute and second from the GPS using the NAV-TIMEUTC
+ * message.
+ */
+void gps_get_time()
+{
+    GPSerror = 0;
+    Serial.flush();
+    // Send a NAV-TIMEUTC message to the receiver
+    uint8_t request[8] = {0xB5, 0x62, 0x01, 0x21, 0x00, 0x00,
+        0x22, 0x67};
+     sendUBX(request, 8);
+
+    // Get the message back from the GPS
+    gps_get_data();
+
+    // Verify the sync and header bits
+    if( buf[0] != 0xB5 || buf[1] != 0x62 )
+        GPSerror = 31;
+    if( buf[2] != 0x01 || buf[3] != 0x21 )
+        GPSerror = 32;
+
+    if( !_gps_verify_checksum(&buf[2], 24) ) {
+      GPSerror = 33;
+    }
+    
+    if(GPSerror == 0) {
+      if(hour > 23 || minute > 59 || second > 59)
+      {
+        GPSerror = 34;
+      }
+      else {
+        hour = buf[22];
+        minute = buf[23];
+        second = buf[24];
+        total_time = hour + minute + second;
+      }
+    }
+}
+
+void prepData() {
+  gps_get_position();
+  gps_get_time();
+  count++;
+  battV = analogRead(2);
+  intTemp = getTemp();
+  n=sprintf (superbuffer, "$$PICO,%d,%02d:%02d:%02d,%ld,%ld,%ld,%d,%d,%d", count, hour, minute, second, lat, lon, alt, sats, battV, intTemp);
+  n = sprintf (superbuffer, "%s*%04X\n", superbuffer, gps_CRC16_checksum(superbuffer));
+}
+
+//From http://arduino.cc/playground/Main/InternalTemperatureSensor
+int getTemp(void)
+{
+  unsigned int wADC;
+
+  // The internal temperature has to be used
+  // with the internal reference of 1.1V.
+  // Channel 8 can not be selected with
+  // the analogRead function yet.
+
+  // Set the internal reference and mux.
+  ADMUX = (_BV(REFS1) | _BV(REFS0) | _BV(MUX3));
+  ADCSRA |= _BV(ADEN);  // enable the ADC
+
+  delay(20);            // wait for voltages to become stable.
+
+  ADCSRA |= _BV(ADSC);  // Start the ADC
+
+  // Detect end-of-conversion
+  while (bit_is_set(ADCSRA,ADSC));
+
+  // Reading register "ADCW" takes care of how to read ADCL and ADCH.
+  wADC = ADCW;
+
+  // The returned temperature is raw.
+  return (wADC);
+}
+
+void setup() {
+  pinMode(7, OUTPUT);
+  pinMode(6, OUTPUT);
+  digitalWrite(7, HIGH);
+  Serial.begin(9600);
+  delay(500);
+  gpsPower(0);
+  setupRadio();
+  startGPS = millis();
+}
+
+void loop() {
+  
+  radio1.write(0x07, 0x08); // turn tx on
+  delay(500);
+  if(gpsstatus == 1){
+    radio1.write(0x07, 0x01); // turn tx off
+    delay(250);
+    radio1.write(0x07, 0x08); // turn tx on
+    delay(500);
+  }
+  radio1.write(0x07, 0x01); // turn tx off
+  delay(10000);
+  
+  if(millis() - startGPS > 300000){
+    if(gpsstatus == 0){
+      gpsPower(1); //turn GPS on
+    }
+    else {
+      gps_check_lock();
+  
+      if( lock == 0x03 || lock == 0x04 )
+      {
+        gps_get_time();
+
+        //This is the daytime loop, operates between 0700 and 2000
+        // 2 situations will break out of this loop - either outside the time
+        // or that we've lost gps lock (though we give it 10 loops in an attempt
+        // to regain lock)
+        while(hour > 6 && hour < 20) {
+          gps_check_lock();
+          if (lock == 0x03 || lock == 0x04) {
+            lockcount = 0;
+          }
+          else {
+            lockcount++;
+            if (lockcount > 10){
+              lockcount = 0;
+              break;
+            }
+          }
+          
+          prepData();
+          //Occasionally the GPS doesn't properly respond with useful data, we need to try and filter
+          // this out - here we look for a change in latitude (which should occur even at rest due to 
+          // GPS drift and also we'll look for a change in time (total_time = hour+mins+secs to create
+          // a relatively unique number).
+          if (lat != oldLat && total_time != old_total_time ) {
+            radio1.write(0x07, 0x08); // turn tx on`
+            rtty_txstring(superbuffer);
+            delay(1000);
+          }
+          oldLat = lat;
+          old_total_time = total_time;
+          
+          if (count % 50 == 0){
+            PSMgps(); //re do power saving setup, currently only sets to Eco as low power modes are unstable
+          }
+        } //End of the daytime loop
+        
+        //This is the night time and default setup
+        gpsPower(0); //turn GPS off
+        prepData();
+        radio1.write(0x07, 0x08); // turn tx on
+        hellsendmsg(superbuffer);
+        startGPS = millis();
+      }
+    }
+    
+  }
+    
+}
