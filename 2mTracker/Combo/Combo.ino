254a9b1bb0ebf7069a72708aecd712e366efefa4
James Coxon <jacoxon@googlemail.com>
2012-05-30 22:33:39 +0100

    added in APRS - not yet tested as hardware mid-build - will attempt to test tomorrow. Does however run on stock arduino328

--- 2mTracker/Combo/Combo.ino
+++ 2mTracker/Combo/Combo.ino
@@ -33,6 +33,27 @@
 #include <string.h>
 #include <stdio.h>
 #include <stdarg.h>
+#include <avr/pgmspace.h>    // needed for PROGMEM
+#include "ax25modem.h"
+
+static const uint8_t PROGMEM _sine_table[] = {
+#include "sine_table.h"
+};
+
+#define BAUD_RATE      (1200)
+#define TABLE_SIZE     (512)
+#define PREAMBLE_BYTES (25)
+#define REST_BYTES     (5)
+
+#define PLAYBACK_RATE    (F_CPU / 256)
+#define SAMPLES_PER_BAUD (PLAYBACK_RATE / BAUD_RATE)
+#define PHASE_DELTA_1200 (((TABLE_SIZE * 1200L) << 7) / PLAYBACK_RATE)
+#define PHASE_DELTA_2200 (((TABLE_SIZE * 2200L) << 7) / PLAYBACK_RATE)
+#define PHASE_DELTA_XOR  (PHASE_DELTA_1200 ^ PHASE_DELTA_2200)
+
+// Data to be transmitted
+volatile static uint8_t *_txbuf = 0;
+volatile static uint8_t  _txlen = 0;
 
 #define ONEPPM 1.0e-6
 #define DEBUG false
@@ -416,15 +437,229 @@ void readElements(int x)//order in the array above
          getElement(elements[x][2],18,25), getElement(elements[x][2],27,33) * 1.0e-7, getElement(elements[x][2],35,42), getElement(elements[x][2],44,51), getElement(elements[x][2],53,63), 
          getElement(elements[x][1],34,43), (getElement(elements[x][2],64,68) + ONEPPM), 0); 
  }
+ 
+  
+ void tx_aprs()
+{
+	char slat[5];
+	char slng[5];
+	char stlm[9];
+	static uint16_t seq = 0;
+	
+	/* Convert the UBLOX-style coordinates to
+	 * the APRS compressed format */
+	lat = 900000000 - lat;
+	lat = lat / 26 - lat / 2710 + lat / 15384615;
+        Serial.print(lat);
+	Serial.print(" ");
+	lon = 900000000 + lon / 2;
+	lon = lon / 26 - lon / 2710 + lon / 15384615;
+        Serial.println(lon);
+	
+	alt = alt / 1000 * 32808 / 10000;
+	
+	/* Construct the compressed telemetry format */
+	ax25_base91enc(stlm + 0, 2, seq);
+	
+	ax25_frame(
+		APRS_CALLSIGN, APRS_SSID,
+		"ARISS", 0,
+		//0, 0, 0, 0,
+                "WIDE1", 1, 0,0,
+		//"WIDE2", 1,
+		"!/%s%sO   /A=%06ld|%s|",
+		ax25_base91enc(slat, 4, lat),
+		ax25_base91enc(slng, 4, lon),
+		alt, stlm
+	);
+	
+	seq++;
+}
+
+ISR(TIMER2_OVF_vect)
+{
+	static uint16_t phase  = 0;
+	static uint16_t step   = PHASE_DELTA_1200;
+	static uint16_t sample = 0;
+	static uint8_t rest    = PREAMBLE_BYTES + REST_BYTES;
+	static uint8_t byte;
+	static uint8_t bit     = 7;
+	static int8_t bc       = 0;
+	
+	/* Update the PWM output */
+	OCR2A = pgm_read_byte(_sine_table[(phase >> 7) & 0x1FF]);
+	phase += step;
+	
+	if(++sample < SAMPLES_PER_BAUD) return;
+	sample = 0;
+	
+	/* Zero-bit insertion */
+	if(bc == 5)
+	{
+		step ^= PHASE_DELTA_XOR;
+		bc = 0;
+		return;
+	}
+	
+	/* Load the next byte */
+	if(++bit == 8)
+	{
+		bit = 0;
+		
+		if(rest > REST_BYTES || !_txlen)
+		{
+			if(!--rest)
+			{
+				/* Disable radio and interrupt */
+				//PORTA &= ~TXENABLE;
+				TIMSK2 &= ~_BV(TOIE2);
+				
+				/* Prepare state for next run */
+				phase = sample = 0;
+				step  = PHASE_DELTA_1200;
+				rest  = PREAMBLE_BYTES + REST_BYTES;
+				bit   = 7;
+				bc    = 0;
+				
+				return;
+			}
+			
+			/* Rest period, transmit ax.25 header */
+			byte = 0x7E;
+			bc = -1;
+		}
+		else
+		{
+			/* Read the next byte from memory */
+			byte = *(_txbuf++);
+			if(!--_txlen) rest = REST_BYTES + 2;
+			if(bc < 0) bc = 0;
+		}
+	}
+	
+	/* Find the next bit */
+	if(byte & 1)
+	{
+		/* 1: Output frequency stays the same */
+		if(bc >= 0) bc++;
+	}
+	else
+	{
+		/* 0: Toggle the output frequency */
+		step ^= PHASE_DELTA_XOR;
+		if(bc >= 0) bc = 0;
+	}
+	
+	byte >>= 1;
+}
+
+void ax25_init(void)
+{
+	/* Fast PWM mode, non-inverting output on OC2A */
+	TCCR2A = _BV(COM2A1) | _BV(WGM21) | _BV(WGM20);
+	TCCR2B = _BV(CS20);
+	
+	/* Make sure radio is not enabled */
+	//PORTA &= ~TXENABLE;
+	
+	/* Enable pins for output (Port A pin 4 and Port D pin 7) */
+	//DDRA |= TXENABLE;
+	pinMode(11, OUTPUT);
+}
+
+static uint8_t *_ax25_callsign(uint8_t *s, char *callsign, char ssid)
+{
+	char i;
+	for(i = 0; i < 6; i++)
+	{
+		if(*callsign) *(s++) = *(callsign++) << 1;
+		else *(s++) = ' ' << 1;
+	}
+	*(s++) = ('0' + ssid) << 1;
+	return(s);
+}
+
+void ax25_frame(char *scallsign, char sssid, char *dcallsign, char dssid,
+		char *path1, char ttl1, char *path2, char ttl2, char *data, ...)
+{
+	static uint8_t frame[100];
+	uint8_t *s;
+	uint16_t x;
+	va_list va;
+	
+	va_start(va, data);
+	
+	/* Pause while there is still data transmitting */
+	while(_txlen);
+	
+	/* Write in the callsigns and paths */
+	s = _ax25_callsign(frame, dcallsign, dssid);
+	s = _ax25_callsign(s, scallsign, sssid);
+	if(path1) s = _ax25_callsign(s, path1, ttl1);
+	if(path2) s = _ax25_callsign(s, path2, ttl2);
+	
+	/* Mark the end of the callsigns */
+	s[-1] |= 1;
+	
+	*(s++) = 0x03; /* Control, 0x03 = APRS-UI frame */
+	*(s++) = 0xF0; /* Protocol ID: 0xF0 = no layer 3 data */
+	
+	vsnprintf((char *) s, 100 - (s - frame) - 2, data, va);
+	va_end(va);
+	
+	/* Calculate and append the checksum */
+	for(x = 0xFFFF, s = frame; *s; s++)
+		x = _crc_ccitt_update(x, *s);
+	
+	*(s++) = ~(x & 0xFF);
+	*(s++) = ~((x >> 8) & 0xFF);
+	
+	/* Point the interrupt at the data to be transmit */
+	_txbuf = frame;
+	_txlen = s - frame;
+	
+	/* Enable the timer and key the radio */
+	TIMSK2 |= _BV(TOIE2);
+	//PORTA |= TXENABLE;
+}
+
+char *ax25_base91enc(char *s, uint8_t n, uint32_t v)
+{
+	/* Creates a Base-91 representation of the value in v in the string */
+	/* pointed to by s, n-characters long. String length should be n+1. */
+	
+	for(s += n, *s = '\0'; n; n--)
+	{
+		*(--s) = v % 91 + 33;
+		v /= 91;
+	}
+	
+	return(s);
+}
+
+void send_APRS() {
+    //Serial.print("Sending APRS");
+    digitalWrite(5, HIGH);
+    delay(1000);
+    tx_aprs();
+    delay(1000);
+    digitalWrite(5, LOW);
+    //Serial.println(" Done");
+}
 
 void setup() {
+  Serial.begin(9600);
   analogReference(DEFAULT);
   pinMode(A3, OUTPUT); //Radio SDN
   digitalWrite(A3, LOW); // Turn on Radio
   setupRadio(); 
-  Serial.begin(9600);
+  
   delay(1000);
   setupGPS();
+  ax25_init();
+  delay(5000);
+  send_APRS();
+  //Serial.print("Finished Setup");
   
 }
 
@@ -456,6 +691,7 @@ void loop() {
     if (elevation >= 5){
       aprs_status = 1;
       //Transmit APRS data now
+      send_APRS();
     }
     else {
       aprs_status = 0;
